f4574afb20c08b2e7b6e0f26315382d441563ba3
Sri Sai Charan Valam <108255901+Killeren@users.noreply.github.com>
2025-07-27 20:15:44 +0530

    Set PeerJS host to public IPv4 address

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ async function startScreenShare() {
         
         // Initialize PeerJS connection
         peer = new Peer(sessionId, {
-            host: '2401:4900:8fdc:d9d8:f8c8:59c8:ea41:4a47',
+            host: '223.181.15.65',
             port: 9000,
             path: '/',
             secure: false,
@@ -205,7 +205,7 @@ function viewScreen() {
         
         // Initialize PeerJS connection
         peer = new Peer(viewerId, {
-            host: '2401:4900:8fdc:d9d8:f8c8:59c8:ea41:4a47',
+            host: '223.181.15.65',
             port: 9000,
             path: '/',
             secure: false,
